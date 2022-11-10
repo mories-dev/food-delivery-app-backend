4fd543a640332cd988439001342be972b89e2596
mories-dev <mories1223@gmail.com>
2022-11-10 09:21:51 +0900

    create処理実装

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd84ffa213f1bec7b32ac855aeb663d78'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d6b1ac1-f655-47c9-a7e9-9cbfc72c6785'
+};
